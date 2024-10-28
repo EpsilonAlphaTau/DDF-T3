933f66b27d1a45c94473543bd23804794f13043c
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-10-28 18:29:02 +0100

    Update js.js

--- JS/js.js
+++ JS/js.js
@@ -521,7 +521,22 @@ function GetCode() {
 	//console.info(">>>Code " + code);
 	code = rand + code + GetLetter(nbTrue);
 	//console.info(">>>>>>Code " + code);
+	document.cookie = 'ddfpwd='+code
 	return code;
+}
+
+console.info("COOK" + getCookie('ddfpwd'));
+
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
 }
 
 function GetDecode(code){
