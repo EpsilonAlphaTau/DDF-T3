a5277f0d8729876fa0955afbb648a17f5e6fd502
EpsilonAlphaTau <jchristophe.guerin@hotmail.fr>
2024-10-31 19:50:33 +0100

    jorda

---
+++ JS/INCLUDES/aveuxceremoniesjorda.js
@@ -0,0 +1,2 @@
+readJS('C','','',"<i>Notre culte est celui de Fulnaïr. Il devait nous libérer des indigeants et des parasites... La société aurait été parfaite, nous aurions accompli de grandes choses. <br/>Nous appelions le feu sombre pour préparer sa venue, en utilisant des rites qui ouvrent l'Outre-Monde.</i>",'Aveux de Jorda',245, 'aveux Jorda cérémonies');
+links('')
